989dee8b87dfaa58d91f8ecc2ae9cd3dadea6a8e
LOKAMITHRA <slokamithra3@gmail.com>
2025-04-24 21:22:04 +0530

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,15 @@
+particlesJS("particles-js", {
+    particles: {
+      number: { value: 100 },
+      size: { value: 3 },
+      color: { value: "#ffffff" },
+      line_linked: {
+        enable: true,
+        color: "#ffffff"
+      },
+      move: {
+        speed: 2
+      }
+    }
+  });
+  
